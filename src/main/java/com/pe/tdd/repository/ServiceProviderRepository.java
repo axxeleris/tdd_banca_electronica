3bf7795e89f0d6f150c4c3c857424323c1f1b755
Sergio Serrano <sergio.serrano@bsdenterprise.com>
2019-01-13 14:36:43 -0600

    Implementacion pruebas de pago de servicios

---
+++ src/main/java/com/pe/tdd/repository/ServiceProviderRepository.java
@@ -0,0 +1,13 @@
+package com.pe.tdd.repository;
+
+import com.pe.tdd.domain.ServiceProvider;
+
+import java.util.List;
+
+public class ServiceProviderRepository {
+
+    public List<ServiceProvider> findAll() {
+        return null;
+    }
+
+}
