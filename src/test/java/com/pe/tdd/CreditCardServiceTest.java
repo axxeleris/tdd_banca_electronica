b1ec00a4ea19f22523423ec837fd3e18875e9c3d
Sergio Serrano <sergio.serrano@bsdenterprise.com>
2019-01-16 11:05:25 -0600

    Implementacion de pruebas de pago de tarjeta de credito

--- src/test/java/com/pe/tdd/CreditCardServiceTest.java
+++ src/test/java/com/pe/tdd/CreditCardServiceTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.mock;
 
 public class CreditCardServiceTest {
 
-    CreditCardService creditCardService;
+    private CreditCardService creditCardService;
 
     private AccountActivityRepository accountActivityRepository;
     private AccountActivityService accountActivityService;
